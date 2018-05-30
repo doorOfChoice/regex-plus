93c516d9ac9c3e4928147121a817ae0524366196
dawndevil <kevinkiller766@gmail.com>
2018-05-30 21:58:56 +0800

    update : 创建CommonAnalyzer

---
+++ src/regexp/born/analyzers/CommonAnalyzer.java
@@ -0,0 +1,20 @@
+package regexp.born.analyzers;
+
+import regexp.MetaPattern;
+import regexp.born.solvers.AbstractSolver;
+import regexp.born.solvers.CommonSolver;
+import regexp.born.solvers.CoreSolver;
+
+public class CommonAnalyzer implements Analyzer {
+    @Override
+    public boolean satisfy(MetaPattern mp) {
+        return true;
+    }
+
+    @Override
+    public AbstractSolver gain(MetaPattern mp, CoreSolver r) {
+        AbstractSolver solver = new CommonSolver(mp.cur());
+        mp.incr();
+        return solver;
+    }
+}
