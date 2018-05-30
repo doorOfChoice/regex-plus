2c4390b6c2f974858dfe6522331d7865f1a7d32e
dawndevil <kevinkiller766@gmail.com>
2018-05-30 21:36:31 +0800

    update : 添加分析器

---
+++ src/regexp/born/analyzers/Analyzer.java
@@ -0,0 +1,23 @@
+package regexp.born.analyzers;
+
+import regexp.MetaPattern;
+import regexp.born.solvers.AbstractSolver;
+
+/**
+ * 分析器用来灵活的定义属于自己的正则表达式
+ */
+public interface Analyzer  {
+    /**
+     * 自定义条件
+     * @param mp 正则单词解析器
+     * @return 是否满足条件
+     */
+    boolean satisfy(MetaPattern mp);
+
+    /**
+     * 满足条件后返回的解析器
+     * @param mp 正则单词解析器
+     * @return 自定义解析器
+     */
+    AbstractSolver gain(MetaPattern mp);
+}
