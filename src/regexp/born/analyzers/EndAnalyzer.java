38cf423c7b421e52d743a431a59f9638e06e682d
dawndevil <kevinkiller766@gmail.com>
2018-05-30 21:48:34 +0800

    update : 添加$解析器

---
+++ src/regexp/born/analyzers/EndAnalyzer.java
@@ -0,0 +1,18 @@
+package regexp.born.analyzers;
+
+import regexp.MetaPattern;
+import regexp.born.solvers.AbstractSolver;
+import regexp.born.solvers.EndSolver;
+
+public class EndAnalyzer implements Analyzer {
+    @Override
+    public boolean satisfy(MetaPattern mp) {
+        return mp.notEnd() && mp.cur().equals("$");
+    }
+
+    @Override
+    public AbstractSolver gain(MetaPattern mp) {
+        mp.incr();
+        return new EndSolver();
+    }
+}
