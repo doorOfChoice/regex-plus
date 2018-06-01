9513717211559c290ee829738409a087eb2e897f
dawndevil <kevinkiller766@gmail.com>
2018-06-01 16:00:17 +0800

    update : 更新MetaPattern注释

--- src/regexp/born/MetaPattern.java
+++ src/regexp/born/MetaPattern.java
@@ -1,9 +1,13 @@
 package regexp.born;
 
-import regexp.born.solvers.SpecialSolver;
-
 /**
  * 正则字符串的元数据库
+ *
+ * MetaPattern用于快速的获取普通字符和特殊字符，只要以\开头的都为特殊字符
+ * 如果一个字符串为1\23, 则这个字符串会被分割为三个元素, 0, \2, 3
+ * 0,1为普通字符, \2为特殊字符
+ *
+ * 提出转义字符和普通字符的结合，有利于用户构建可扩展的正则表达式
  */
 public class MetaPattern implements Cloneable {
     private int i = 0;
@@ -13,22 +17,26 @@ public class MetaPattern implements Cloneable {
         this.s = s;
     }
 
+    //判断是否能够移动到下一个索引
     public boolean ok() {
         if (isSpecial())
             return i + 2 < s.length();
         return i + 1 < s.length();
     }
 
+    //是否已经结束
     public boolean notEnd() {
         return i < s.length();
     }
 
+    //向右移动并获取字符串
     public String incr() {
         String r = cur();
         i = i + (isSpecial() ? 2 : 1);
         return r;
     }
 
+    //向右移动x单位并获取字符串
     public String incr(int x) {
         String r = cur();
         for (int i = 0; i < x; ++i)
@@ -36,6 +44,7 @@ public class MetaPattern implements Cloneable {
         return r;
     }
 
+    //获取当前字符串
     public String cur() {
         String r;
         if (isSpecial())
@@ -44,6 +53,7 @@ public class MetaPattern implements Cloneable {
 
     }
 
+    //获取前一个字符串
     public String pre() {
         String r;
         if (isSpecial(i - 2))
@@ -54,6 +64,7 @@ public class MetaPattern implements Cloneable {
         return r;
     }
 
+    //获取下一个字符串
     public String next() {
         String r;
         if (isSpecial()) {
@@ -64,6 +75,7 @@ public class MetaPattern implements Cloneable {
         return r;
     }
 
+    //获取下x个字符串
     public String next(int x) {
         int j = i;
         int count = 0;
@@ -77,10 +89,12 @@ public class MetaPattern implements Cloneable {
     }
 
 
-    public boolean isSpecial() {
+
+    private boolean isSpecial() {
         return isSpecial(i);
     }
 
+    //判断当前下标处是否为特殊字符
     private boolean isSpecial(int v) {
         return v >= 0 && v + 1 < s.length() && s.charAt(v) == '\\';
     }
